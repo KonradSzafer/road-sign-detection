da3d356960f21e6c77f064dddda80e81ad7598bc
KonradSzafer <szafer.konrad@gmail.com>
2022-02-14 20:44:53 +0100

    refactored data loading

--- road_signs_detection/image_utils.py
+++ road_signs_detection/image_utils.py
@@ -5,7 +5,11 @@ from torchvision import transforms
 
 
 def get_bounding_box(row):
-    return np.array([row[4], row[6]], row[5], row[7])
+    return np.array([
+        row['xmin'],
+        row['xmax'],
+        row['ymin'],
+        row['ymax']])
 
 
 def resize_img_and_bb(img_path, bounding_box, new_width, new_height):
@@ -13,17 +17,18 @@ def resize_img_and_bb(img_path, bounding_box, new_width, new_height):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_width, img_height, _ = img.shape
-    plt.imshow(img)
-    plt.show()
 
     bb_x_min, bb_x_max, bb_y_min, bb_y_max = bounding_box
     mask = np.zeros((img_width, img_height, 3))
     cv2.rectangle(mask, (bb_x_min, bb_y_min), (bb_x_max, bb_y_max), (255,0,0), 1)
-    plt.imshow(mask)
-    plt.show()
 
     img = cv2.resize(img, dsize=(new_width, new_height), interpolation=cv2.INTER_NEAREST)
+    # plt.imshow(img)
+    # plt.show()
     mask = cv2.resize(mask, dsize=(new_width, new_height), interpolation=cv2.INTER_NEAREST)
+    # plt.imshow(mask)
+    # plt.show()
+
     cols, rows = np.nonzero(mask[:,:,0])
     bounding_box = np.array([np.min(rows),
                             np.max(rows),
